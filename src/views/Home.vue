da81e0dfb0bc8ee8756c6fa3082285481d21eae9
Guilherme Burger <origgin@gmail.com>
2021-07-04 16:23:18 -0300

    Solved src eslint problem from import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Test />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import Test from "@/components/Test.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    Test,
   },
 };
 </script>
